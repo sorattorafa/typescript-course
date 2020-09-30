1c350db4b7c1dd1d3af36df41cd49f97f11b366a
sorattorafa <rafael.soratto@appmoove.com.br>
2020-09-30 17:28:31 -0300

    feat: ensure AccountMongoRepository returns an account on sucess

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,14 +1,20 @@
-import { MongoClient } from 'mongodb'
+import { Collection, MongoClient } from 'mongodb'
 
 export const MongoHelper = {
   client: null as MongoClient,
+
   async connect (uri: string): Promise<void> {
     this.client = await MongoClient.connect(process.env.MONGO_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     })
   },
+
   async disconnect () {
     await this.client.close()
+  },
+
+  getCollection (name: String): Collection {
+    return this.client.db().collection(name)
   }
 }

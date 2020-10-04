e66b8789f3c4f27fc4f81c9919324e9d35852c02
rafael_soratto <rafael.soratto@appmoove.com.br>
2020-10-04 00:24:33 -0300

    refactor: create factory

--- src/infra/db/mongodb/log-repository/log-spec.ts
+++ src/infra/db/mongodb/log-repository/log-spec.ts
@@ -1,7 +1,9 @@
 import { MongoHelper } from '../helpers/mongo-helper'
 import { Collection } from 'mongodb'
 import { LogMongoRepository } from './log'
-
+const makeSut = (): LogMongoRepository => {
+  return new LogMongoRepository()
+}
 describe('Log Mongo Repository', () => {
   let errorCollection: Collection
   beforeAll(async () => {
@@ -18,7 +20,7 @@ describe('Log Mongo Repository', () => {
   })
 
   test('Should create an error log on sucess', async () => {
-    const sut = new LogMongoRepository()
+    const sut = makeSut()
     await sut.logError('any_error')
     const count = await errorCollection.countDocuments()
     expect(count).toBe(1)

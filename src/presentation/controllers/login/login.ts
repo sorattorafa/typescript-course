428f74214df5b7e4b1384aa69fe189d963220007
rafael_soratto <rafael.soratto@appmoove.com.br>
2020-10-04 14:31:25 -0300

    feat: ensure LoginController calls EnailValidator with correct email

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,13 +1,26 @@
 import { HttpRequest, HttpResponse, Controller } from '../../protocols'
 import { badRequest } from '../../helpers/http-helper'
 import { MissingParamError } from '../errors'
+import { EmailValidator } from '../signup/signup-protocols'
 export class LoginController implements Controller {
+  private readonly emailValidator: EmailValidator
+  constructor (emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    if (!httpRequest.body.email) {
+    const {
+      body: {
+        email,
+        password
+      }
+    } = httpRequest
+    if (!email) {
       return new Promise(resolve => resolve(badRequest(new MissingParamError('email'))))
     }
-    if (!httpRequest.body.password) {
+    if (!password) {
       return new Promise(resolve => resolve(badRequest(new MissingParamError('password'))))
     }
+    this.emailValidator.isValid(email)
   }
 }
